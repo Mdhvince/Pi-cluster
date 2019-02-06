0f6f151f2881be46415258a26aa7d9667bef93e0
Medhy Vinceslas <vinceslas.medhy@gmail.com>
2019-02-06 18:35:46 +0100

    allow connection for each pi

--- script.sh
+++ script.sh
@@ -1,4 +1,13 @@
 # install sshpass using brew
 brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb
 
-# for each Raspi : One by One
+#---for each Raspi : One by One---#
+
+# allow connection
+ssh pi@192.168.1.X # then tape yes and enter password
+
+# change the hostname
+sudo nano /etc/hosts # replace raspberrypi by the name (ie: rpi01), ctrl+x and return
+sudo nano /etc/hostname # same as above
+sudo reboot
+

0b9dded5a5b96105571ce221551bbca56785f58e
Medhy Vinceslas <vinceslas.medhy@gmail.com>
2019-02-14 09:02:09 +0100

    shell script simplified

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 # install sshpass using brew
 brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb
 
-#---for each Raspi : One by One---#
+#---for each Raspi---#
 
 # allow connection
 ssh pi@192.168.1.X # then tape yes and enter password
@@ -18,26 +18,16 @@ ssh pi@rpi-worker01.local # tape yes and quit
 #---INSTALL ALL THE NECESSARY PACKAGE ON ALL RASPI---#
 # We can now use sshpass with the password as argument
 
-for i in rpi-worker01 rpi-worker02 rpi-worker03 rpi-worker04 ; do sshpass -p 'raspberry' ssh pi@$i.local -yes "sudo apt-get update && sudo apt-get dist-upgrade; sudo apt-get install python3-pip; pip3 install virtualenv; python3 -m virtualenv env; . env/bin/activate; pip install dask[complete] distributed bokeh --upgrade; deactivate"; done
+sshpass -p 'yourpassword' ssh pi@$i.local -yes
+sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get install python3-pip
+pip3 install virtualenv
+python3 -m virtualenv env
+. env/bin/activate
+sudo apt-get install libatlas-base-dev
+pip install dask[complete] distributed bokeh --upgrade
+pip install s3fs
 
-# THE SAME CODE AS ABOVE
-for i in rpi-worker01 rpi-worker02 rpi-worker03 rpi-worker04
-do
-	sshpass -p 'raspberry' ssh pi@$i.local -yes
-	sudo apt-get update && sudo apt-get dist-upgrade
-	sudo apt-get install python3-pip
-	pip3 install virtualenv
-	python3 -m virtualenv env
-	. env/bin/activate
-	sudo apt-get install libatlas-base-dev
-	pip install dask[complete] distributed bokeh --upgrade
-	pip install s3fs
-	deactivate
-done
-
-# You can also type command without for loop and just synchronize terminal panes when entering the commande.
-
-
-#---for each Raspi : One by One---#
-# LAUNCH WORKERS (for each line) - there is a for loop in order to be able to kill process
-for i in rpi-worker01 ; do sshpass -p 'raspberry' ssh pi@$i.local -yes ". env/bin/activate; dask-worker YourScheduler"; done
+# Launch the pi as a Worker
+. env/bin/activate
+dask-worker YourSchedulerAdress
